595d70f1e87cbae63e3e6f01b1a7324acc97f914
TubaAricam <84075546+TubaAricam@users.noreply.github.com>
2022-06-19 16:08:29 +0300

    Create sqlhmw6.sql

---
+++ sqlhmw6.sql
@@ -0,0 +1,11 @@
+1.film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+select AVG(rental_rate) from film;
+
+2.film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+select COUNT(title) from film WHERE title LIKE 'C%';
+
+3.film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+select * from film WHERE rental_rate=0.99 ORDER BY length DESC LIMIT 1 ;
+
+4.film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+select COUNT(DISTINCT replacement_cost) from film WHERE length>150;
